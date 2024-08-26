5fd7764ff48fd548bdfcdd55f9ea8fe9b3a7be83
calebewerneckcouto <calebewerneck@hotmail.com>
2024-08-26 19:21:48 -0300

    Desafio Consulta Vendas

---
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dsmeta.controllers;
+
+import com.devsuperior.dsmeta.dto.ReportMinDTO;
+import com.devsuperior.dsmeta.dto.SummaryMinDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.services.SaleService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
+		SaleMinDTO dto = service.findById(id);
+		return ResponseEntity.ok(dto);
+	}
+
+	@GetMapping(value = "/report")
+	public ResponseEntity<Page<ReportMinDTO>> getReport(
+			@RequestParam(value = "minDate", required = false, defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", required = false, defaultValue = "") String maxDate,
+			@RequestParam(value = "name", required = false, defaultValue = "") String name,
+			Pageable pageable) {
+		Page<ReportMinDTO> dto = service.findByReport(minDate,maxDate,name,pageable);
+		return ResponseEntity.ok(dto);
+	}
+
+	@GetMapping(value = "/summary")
+	public ResponseEntity<List<SummaryMinDTO>> getSummary(
+			@RequestParam(name = "minDate", defaultValue = "")String minDate,
+			@RequestParam(name = "maxDate", defaultValue = "")String maxDate) {
+		List<SummaryMinDTO> dto = service.findBySummary(minDate,maxDate);
+		return ResponseEntity.ok(dto);
+	}
+}
---
+++ src/main/java/com/devsuperior/dsmeta/dto/SummaryMinDTO.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsmeta.dto;
+
+
+import com.devsuperior.dsmeta.projections.SummaryMinProjection;
+
+
+public class SummaryMinDTO {
+    private String sellerName;
+    private Double total;
+
+    public SummaryMinDTO(String sellerName, Double total) {
+        this.sellerName = sellerName;
+        this.total = total;
+    }
+
+    public SummaryMinDTO(SummaryMinProjection projection){
+        sellerName = projection.sellerName();
+        total = projection.total();
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+}
